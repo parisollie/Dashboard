0b122eef52a30b21f830f322f573194280768074
parisollie <pjff18@gmail.com>
2023-12-18 13:01:38 -0600

    Settings

--- app/src/main/java/com/pjff/dashboard/Home.kt
+++ app/src/main/java/com/pjff/dashboard/Home.kt
@@ -1,10 +1,17 @@
 package com.pjff.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import com.pjff.dashboard.activities.SettingsActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,7 +23,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Home.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Home : Fragment() {
+/*class Home : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -56,4 +63,58 @@ class Home : Fragment() {
                 }
             }
     }
+}*/
+
+class Home : Fragment() {
+
+    //private lateinit var binding:
+
+    // TODO Step 5: Override the onCreate function and add the piece of code to inflate the option menu in fragment.
+    // START
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // If we want to use the option menu in fragment we need to add it.
+        setHasOptionsMenu(true)
+    }
+    // END
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        textView.text = "This is dashboard Fragment"
+        return root
+    }
+
+    // TODO Step 6: Override the onCreateOptionMenu function and inflate the Dashboard menu file init.
+    // START
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.dashboard_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+    // END
+
+    // TODO Step 7: Override the onOptionItemSelected function and handle the action items init.
+    // START
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when (id) {
+
+            R.id.action_settings -> {
+
+                // TODO Step 9: Launch the SettingActivity on click of action item.
+                // START
+                startActivity(Intent(activity, SettingsActivity::class.java))
+                // END
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    // END
 }
---
+++ app/src/main/java/com/pjff/dashboard/activities/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.pjff.dashboard.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pjff.dashboard.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
